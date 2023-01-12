8c68fcdc6b69211b5a201d8b815e2fc58165c6fc
Jan-Lukas Christmann <info@it-christmann.de>
2023-01-12 18:31:10 +0100

    Projekt ini

---
+++ src/main/java/de/itchristmann/ICERP/IcErpApplication.java
@@ -0,0 +1,13 @@
+package de.itchristmann.ICERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/itchristmann/ICERP/IcErpApplicationTests.java
@@ -0,0 +1,13 @@
+package de.itchristmann.ICERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
